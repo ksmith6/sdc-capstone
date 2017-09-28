c9341f4351d35717b783f1c876bbfe8b3499d16a
luos <shaobo.luo@gmail.com>
2017-09-28 17:55:18 +0800

    modify the tl_classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,8 +1,12 @@
 from styx_msgs.msg import TrafficLight
+import cv2
+from keras.models import load_model
+from numpy import zeros, newaxis
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
+        model = load_model('test_1.h5')
+        print(model)
         pass
 
     def get_classification(self, image):
@@ -15,5 +19,19 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+        imrs = imrs.astype(float)
+    	imrs = imrs / 255.0
+
+    	imrs = imrs[newaxis,:,:,:]
+        print(imrs)
+    	preds = model.predict(imrs)
+    	print('Predicted:' ,preds)
+    	predicted_class = np.argmax(preds, axis=1)
+
+    	print('Predicted C:' ,predicted_class)
+    	lid = predicted_class[0]
+
+        if(lid == 1):
+           return TrafficLight.RED
+
         return TrafficLight.UNKNOWN
