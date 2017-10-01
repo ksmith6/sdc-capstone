c279d77ea50f2827d84360e58496663036178165
luos <shaobo.luo@gmail.com>
2017-10-01 22:01:48 +0800

    add readme

---
+++ ros/src/tl_detector/train.py
@@ -0,0 +1,127 @@
+from keras import applications
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.models import Sequential, Model
+from keras.layers import Dropout, Flatten, Dense, GlobalAveragePooling2D
+from keras import backend as k
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
+from keras.applications.inception_v3 import InceptionV3
+from keras.layers import Conv2D, Flatten, MaxPooling2D, Activation
+from keras.models import Sequential
+from keras.layers import Dense, Input, Activation
+
+img_width, img_height = 400, 400
+path ='/home/luos/uda/tflight/'
+train_data_dir = path +"img"
+validation_data_dir =  path +"img"
+nb_train_samples = 1233
+nb_validation_samples = 1233
+batch_size = 32
+epochs = 100
+
+img_rows, img_cols = img_height, img_width
+# number of convolutional filters to use
+#nb_filters = 32
+nb_filters = 32
+# size of pooling area for max pooling
+pool_size = (2, 2)
+# convolution kernel size
+kernel_size = (3, 3)
+model = Sequential()
+model.add(Conv2D(nb_filters, kernel_size[0], kernel_size[1],
+                        border_mode='valid',
+                        input_shape=(img_height, img_width, 3)))
+model.add(Activation('relu'))
+
+model.add(Conv2D(nb_filters, kernel_size[0], kernel_size[1]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=pool_size))
+
+model.add(Conv2D(nb_filters, kernel_size[0], kernel_size[1]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=pool_size))
+
+model.add(Conv2D(nb_filters, kernel_size[0], kernel_size[1]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=pool_size))
+
+model.add(Conv2D(nb_filters, kernel_size[0], kernel_size[1]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=pool_size))
+
+model.add(Conv2D(nb_filters, kernel_size[0], kernel_size[1]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=pool_size))
+
+
+model.add(Conv2D(nb_filters, kernel_size[0], kernel_size[1]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=pool_size))
+
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2))
+model.add(Activation('softmax'))
+
+
+model.summary()
+# TODO: Compile and train the model here.
+model.compile(loss='categorical_crossentropy',
+          optimizer='adam',
+          metrics=['accuracy'])
+
+
+
+# Initiate the train and test generators with data Augumentation
+train_datagen = ImageDataGenerator(
+rescale = 1./255,
+horizontal_flip = True,
+fill_mode = "nearest",
+zoom_range = 0.3,
+width_shift_range = 0.3,
+height_shift_range=0.3,
+rotation_range=30)
+
+test_datagen = ImageDataGenerator(
+rescale = 1./255,
+horizontal_flip = True,
+fill_mode = "nearest",
+zoom_range = 0.3,
+width_shift_range = 0.0,
+height_shift_range=0.0,
+rotation_range=30)
+
+train_generator = train_datagen.flow_from_directory(
+train_data_dir,
+target_size = (img_height, img_width),
+batch_size = batch_size,
+class_mode = "categorical")
+
+validation_generator = test_datagen.flow_from_directory(
+validation_data_dir,
+target_size = (img_height, img_width),
+class_mode = "categorical")
+
+# Save the model according to the conditions
+checkpoint = ModelCheckpoint("model.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
+
+
+# Train the model
+model.fit_generator(
+train_generator,
+samples_per_epoch = nb_train_samples,
+epochs = epochs,
+validation_data = validation_generator,
+nb_val_samples = nb_validation_samples,
+callbacks = [checkpoint, early])
+
+
+
+
+import gc;
+gc.collect()
